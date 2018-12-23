eaf0cbbe999570a61d85254d3aa827f3edd83c41
KrakeN0O0 <jenya.love72@gmail.com>
2018-12-23 19:44:30 +0300

    Some changes here...

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -16,9 +16,5 @@ public class ExampleMod
     {
 		// some example code
         System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
-<<<<<<< HEAD
-=======
-
->>>>>>> 14a63c4db00a3e663b5ec026faaddfb8c50e77cd
     }
 }
